7259e8a0028a65a8289a4b0211bc582f036dec61
naser101289 <naser.bd.edu@gmail.com>
2014-09-23 09:32:46 -0700

    Array Example

---
+++ Array/practice111/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace practice111
+{
+    partial class homeTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.displayListBox = new System.Windows.Forms.ListBox();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resetButton);
+            this.groupBox1.Controls.Add(this.countLabel);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.nameLabel);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 120);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(274, 83);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Location = new System.Drawing.Point(111, 93);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(35, 13);
+            this.countLabel.TabIndex = 3;
+            this.countLabel.Text = "Count";
+            
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(274, 41);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(40, 47);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(114, 43);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(137, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // displayListBox
+            // 
+            this.displayListBox.FormattingEnabled = true;
+            this.displayListBox.Location = new System.Drawing.Point(12, 185);
+            this.displayListBox.Name = "displayListBox";
+            this.displayListBox.Size = new System.Drawing.Size(366, 121);
+            this.displayListBox.TabIndex = 1;
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(96, 147);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(137, 23);
+            this.showAllButton.TabIndex = 3;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // homeTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 320);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.displayListBox);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "homeTask";
+            this.Text = "Home Task";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.ListBox displayListBox;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Array/practice111/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice111
+{
+    public partial class homeTask : Form
+    {
+        public homeTask()
+        {
+            InitializeComponent();
+        }
+
+        string[] nameInput = new string[10];
+        int i = 0;
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (i < 10)
+            {
+
+                nameInput[i] = nameTextBox.Text;
+                nameTextBox.Text = "";
+                countLabel.Text = (i + 1).ToString();
+                
+                i++;
+
+            }
+
+            else
+            {
+               
+                addButton.Enabled = false;
+                nameTextBox.Text = "";
+
+                foreach (string s in nameInput)
+                {
+                    if (s != null)
+                    {
+                        displayListBox.Items.Add(s);
+                    }
+                }
+
+            }
+
+        }
+
+ 
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            nameInput = new string [10];
+            displayListBox.Items.Clear();
+            i = 0;
+            addButton.Enabled = true;
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            displayListBox.Items.Clear();
+            foreach (string s in nameInput)
+            {
+                if (s != null)
+                {
+                    displayListBox.Items.Add(s);
+                }
+            }
+        }
+    }
+}
