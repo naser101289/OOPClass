636545bdb70c7f5f85735b0455eec402adc459ad
naser101289 <naser.bd.edu@gmail.com>
2014-09-23 09:43:47 -0700

    Array Double Click

---
+++ ArrayDoubleClick/employeeinformation/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace employeeinformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.salaryLabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.detailsListView = new System.Windows.Forms.ListView();
+            this.idColulmnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nameCoolumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.salaryColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(12, 31);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(16, 13);
+            this.idLabel.TabIndex = 0;
+            this.idLabel.Text = "Id";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 57);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // salaryLabel
+            // 
+            this.salaryLabel.AutoSize = true;
+            this.salaryLabel.Location = new System.Drawing.Point(12, 83);
+            this.salaryLabel.Name = "salaryLabel";
+            this.salaryLabel.Size = new System.Drawing.Size(36, 13);
+            this.salaryLabel.TabIndex = 2;
+            this.salaryLabel.Text = "Salary";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(80, 28);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(80, 80);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.salaryTextBox.TabIndex = 4;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(80, 54);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(154, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(80, 149);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // detailsListView
+            // 
+            this.detailsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.idColulmnHeader,
+            this.nameCoolumnHeader,
+            this.salaryColumnHeader});
+            this.detailsListView.FullRowSelect = true;
+            this.detailsListView.GridLines = true;
+            this.detailsListView.Location = new System.Drawing.Point(15, 194);
+            this.detailsListView.Name = "detailsListView";
+            this.detailsListView.Size = new System.Drawing.Size(267, 164);
+            this.detailsListView.TabIndex = 7;
+            this.detailsListView.UseCompatibleStateImageBehavior = false;
+            this.detailsListView.View = System.Windows.Forms.View.Details;
+            this.detailsListView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.detailsListView_MouseDoubleClick);
+            // 
+            // idColulmnHeader
+            // 
+            this.idColulmnHeader.Text = "Id";
+            this.idColulmnHeader.Width = 43;
+            // 
+            // nameCoolumnHeader
+            // 
+            this.nameCoolumnHeader.Text = "Name";
+            this.nameCoolumnHeader.Width = 109;
+            // 
+            // salaryColumnHeader
+            // 
+            this.salaryColumnHeader.Text = "Salary";
+            this.salaryColumnHeader.Width = 111;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(80, 106);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(154, 20);
+            this.addressTextBox.TabIndex = 8;
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(12, 109);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(45, 13);
+            this.addressLabel.TabIndex = 9;
+            this.addressLabel.Text = "Address";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 370);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.detailsListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.salaryLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.idLabel);
+            this.Name = "Form1";
+            this.Text = "Employee Information Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label salaryLabel;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ListView detailsListView;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.ColumnHeader idColulmnHeader;
+        private System.Windows.Forms.ColumnHeader nameCoolumnHeader;
+        private System.Windows.Forms.ColumnHeader salaryColumnHeader;
+    }
+}
+
---
+++ ArrayDoubleClick/employeeinformation/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace employeeinformation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            string address;
+
+            string[] details = new string[3];
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            if (idTextBox.Text != "" && nameTextBox.Text != "" && salaryTextBox.Text != "" && addressTextBox.Text != "" )
+            {
+
+               int id = Convert.ToInt32(idTextBox.Text);
+               string name = nameTextBox.Text;
+               double salary = Convert.ToDouble(salaryTextBox.Text);
+               address = addressTextBox.Text;
+
+                ListViewItem item;
+
+                details[0] = idTextBox.Text;
+                details[1] = nameTextBox.Text;
+                details[2] = salaryTextBox.Text;
+
+                item = new ListViewItem(details);
+                detailsListView.Items.Add(item);
+
+            }
+
+                     idTextBox.Text = "";
+                     nameTextBox.Text = "";
+                     salaryTextBox.Text = "";
+                     addressTextBox.Text = "";
+
+        }
+
+        private void detailsListView_MouseDoubleClick(object sender, MouseEventArgs e)
+
+        {
+            
+            int selectedIndex = detailsListView.SelectedIndices[0];
+
+            idTextBox.Text = detailsListView.Items[selectedIndex].SubItems[0].Text;
+            nameTextBox.Text = detailsListView.Items[selectedIndex].SubItems[1].Text;
+            salaryTextBox.Text = detailsListView.Items[selectedIndex].SubItems[2].Text;
+
+            addressTextBox.Text = address;
+        }
+
+        
+    }
+}
