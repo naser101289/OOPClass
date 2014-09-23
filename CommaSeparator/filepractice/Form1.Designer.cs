ae5f4aa3437110341166b2e3ca558dd167ddc785
naser101289 <naser.bd.edu@gmail.com>
2014-09-23 09:46:41 -0700

    Comma Separator

---
+++ CommaSeparator/filepractice/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace filepractice
+{
+    partial class EmployeeSalary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterNameLabel = new System.Windows.Forms.Label();
+            this.enterNameTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.salaryLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.displayListBox = new System.Windows.Forms.ListBox();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // enterNameLabel
+            // 
+            this.enterNameLabel.AutoSize = true;
+            this.enterNameLabel.Location = new System.Drawing.Point(41, 31);
+            this.enterNameLabel.Name = "enterNameLabel";
+            this.enterNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.enterNameLabel.TabIndex = 0;
+            this.enterNameLabel.Text = "Enter Name";
+            // 
+            // enterNameTextBox
+            // 
+            this.enterNameTextBox.Location = new System.Drawing.Point(125, 28);
+            this.enterNameTextBox.Name = "enterNameTextBox";
+            this.enterNameTextBox.Size = new System.Drawing.Size(287, 20);
+            this.enterNameTextBox.TabIndex = 1;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(125, 100);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(287, 20);
+            this.salaryTextBox.TabIndex = 2;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(125, 64);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(287, 20);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(41, 67);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(16, 13);
+            this.idLabel.TabIndex = 4;
+            this.idLabel.Text = "Id";
+            // 
+            // salaryLabel
+            // 
+            this.salaryLabel.AutoSize = true;
+            this.salaryLabel.Location = new System.Drawing.Point(41, 103);
+            this.salaryLabel.Name = "salaryLabel";
+            this.salaryLabel.Size = new System.Drawing.Size(36, 13);
+            this.salaryLabel.TabIndex = 5;
+            this.salaryLabel.Text = "Salary";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(337, 138);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(337, 184);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 7;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // displayListBox
+            // 
+            this.displayListBox.FormattingEnabled = true;
+            this.displayListBox.Location = new System.Drawing.Point(44, 247);
+            this.displayListBox.Name = "displayListBox";
+            this.displayListBox.Size = new System.Drawing.Size(368, 134);
+            this.displayListBox.TabIndex = 8;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(173, 402);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 9;
+            this.totalLabel.Text = "Total";
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(260, 399);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(152, 20);
+            this.totalTextBox.TabIndex = 10;
+            // 
+            // EmployeeSalary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 437);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.displayListBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.salaryLabel);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.enterNameTextBox);
+            this.Controls.Add(this.enterNameLabel);
+            this.Name = "EmployeeSalary";
+            this.Text = "Employee Salary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterNameLabel;
+        private System.Windows.Forms.TextBox enterNameTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label salaryLabel;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListBox displayListBox;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox totalTextBox;
+    }
+}
+
---
+++ CommaSeparator/filepractice/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filepractice
+{
+    public partial class EmployeeSalary : Form
+    {
+        public EmployeeSalary()
+        {
+            InitializeComponent();
+        }
+
+        private string filelocation = @"F:\employeesalary.txt";
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            int id = Convert.ToInt32(idTextBox.Text);
+            string name = enterNameTextBox.Text;
+            double salary = Convert.ToDouble(salaryTextBox.Text);
+            string aRow = name + "," + id + "," + salary;
+
+            FileStream aStream = new FileStream(filelocation, FileMode.Append);
+            StreamWriter aStreamWriter = new StreamWriter(aStream);
+            aStreamWriter.Write(aRow);
+            aStreamWriter.WriteLine();
+            aStreamWriter.Close();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            FileStream aStream = new FileStream(filelocation, FileMode.Open);
+            StreamReader asStreamReader = new StreamReader(aStream);
+            displayListBox.Items.Clear();
+            double totalSalary = 0;
+
+            while (!asStreamReader.EndOfStream)
+            {
+                string aRow = asStreamReader.ReadLine();
+                char[] separator = {','};
+                string [] employeeInfo = aRow.Split(separator);
+                displayListBox.Items.Add(employeeInfo[0] + " " + employeeInfo[1] + " " + employeeInfo[2]);
+                double salary = Convert.ToInt32(employeeInfo[2]);
+                totalSalary += salary;
+            }
+
+            totalTextBox.Text = totalSalary.ToString();
+            asStreamReader.Close();
+
+        }
+    }
+}
